841bb37070b32b2916541bec4c7d6d591b77d46f
Bhushan <bhushanforu74@gmail.com>
2024-01-24 20:02:07 -0500

    commit

--- emp_tbl_view.php
+++ emp_tbl_view.php
@@ -2,6 +2,7 @@
 include 'dbconnection.php';
 
 $html = "<html><table style='width:100%' border='1px solid black'><tr>
+    <th>id</th>
     <th>Job Title</th>
     <th>Job Description</th>
     <th>Department</th>
@@ -13,14 +14,14 @@ $html = "<html><table style='width:100%' border='1px solid black'><tr>
   </tr>";
 
 // get id
-$sql1 = "SELECT jobTitle, jobDescription, department, requiredSkills, hourlyRate,applicationInstructions, contactInfo, status FROM jobposted WHERE status='open'";
+$sql1 = "SELECT id, jobTitle, jobDescription, department, requiredSkills, hourlyRate,applicationInstructions, contactInfo, status FROM jobposted WHERE status='open'";
 $result = mysqli_query($conn, $sql1);
 
 if (mysqli_num_rows($result) > 0) {
 	    // output data of each row
 	    while($row = mysqli_fetch_assoc($result)) {
 	    	//$price_tax = ( $price * $tax) + $price;
-			$html = $html."<tr><td>".$row['jobTitle']."</td><td>".$row['jobDescription']."</td><td>".$row['department']."</td>
+			$html = $html."<tr><td>".$row['id']."</td><td>".$row['jobTitle']."</td><td>".$row['jobDescription']."</td><td>".$row['department']."</td>
             <td>".$row['requiredSkills']."</td><td>".$row['hourlyRate']."</td><td>".$row['applicationInstructions']."</td><td>".$row['contacInfo']."</td>
             <td>".$row['status']."</td></tr>";
 	    }
--- submit.php
+++ submit.php
@@ -1,10 +1,10 @@
-$sql1 = "SELECT MAX(id) AS id FROM formtest2";
+$sql1 = "SELECT MAX(id) AS id FROM jobposted";
 $result = mysqli_query($conn, $sql1);
 $row = mysqli_fetch_assoc($result);
 $newid = $row['id'] + 1;
 
 
-$sql2 = "INSERT INTO formtest2 (id, firstname, lastname, email)
+$sql2 = "INSERT INTO formtest2 (id, jobTitle, jobDescription, department, requiredSkills, hourlyRate,applicationInstructions, contactInfo, status)
 VALUES ('".$newid."', '".$fname."', '".$lname."', '".$email."')";
 
 if (mysqli_query($conn, $sql2)) {
